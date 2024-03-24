38ba27132b7991dedc7fb82c054800ad217c4c8a
laureannegethro <laureannegethro@localhost>
2024-03-24 18:18:37 -0400

    First commit

---
+++ CSC320-Week1/src/PersonalInformation.java
@@ -0,0 +1,29 @@
+
+public class PersonalInformation {
+
+	public static void main(String[] args) {
+		
+		// Variables declaration 
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        int zipCode;
+        
+        //Value initialization 
+        firstName = "Laure Anne-Gethro";
+        lastName = "Jean";
+        address = "Belvil 3 Boulevard des Entreprenneurs";
+        city = "Petion - Ville";
+        zipCode = 6110;
+
+        //Variables Printing
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("Zipcode: " + zipCode);
+
+	}
+
+}
